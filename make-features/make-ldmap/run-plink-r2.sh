f7de6d938d57600de9f20eda573ff594a0a530ab
Ed Mountjoy <emountjoy@gmail.com>
2015-03-28 13:40:13 +0000

    tidy extras

--- make-features/make-ldmap/run-plink-r2.sh
+++ make-features/make-ldmap/run-plink-r2.sh
@@ -3,13 +3,14 @@
 #PBS -l walltime=00:06:00:00
 #PBS -N n-plink-r2
 #PBS -m abe
-#PBS -M edward.mountjoy@bristol.ac.uk
 ##PBS -q testq
 
 cd $PBS_O_WORKDIR
 
+# Options
 geno_pattern=/panfs/panasas01/sscm/em13383/alspac_data/27Feb2015/data/genotypes/bestguess/data_chrCHROM
 exclude_samples=/panfs/panasas01/sscm/em13383/phd/alspac/data/filter-files/mothers-and-custom-related_150323.txt
+outbfile=unrelated-children_maf0.01_info0.8
 
 # Make merge list
 ml=merge_list.txt
@@ -19,5 +20,4 @@ for chrom in $(seq -w 1 23); do
 done > $ml
 
 # Merge and extract
-outbfile=unrelated-children_maf0.01_info0.8
 time plink --merge-list "$ml" --out "$outbfile" --remove "$exclude_samples" --r2 --ld-window-kb 1000 --ld-window-r2 0.5
